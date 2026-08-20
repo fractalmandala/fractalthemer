212a394c1818416abdf887a2b7e1a594c6dbe15a
FractalMandala <i.amrit.p@gmail.com>
2026-08-20 19:45:16 +0530

    cleanup

--- src/lib/icons/Sun.svelte
+++ src/lib/icons/Sun.svelte
@@ -1,13 +1,4 @@
-<script lang="ts">
-	interface Props {
-		size?: number | string;
-		class?: string;
-	}
-
-	let { size = 16, class: className = '' }: Props = $props();
-</script>
-
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_180_157)">
 <path d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M22 12H23" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -20,8 +11,5 @@
 <path d="M1 12H2" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 </g>
 <defs>
-<clipPath id="clip0_180_157">
-<rect width="24" height="24" fill="white"/>
-</clipPath>
 </defs>
 </svg>
